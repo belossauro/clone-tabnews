8f510f2f5e751d67a7824f163e48b586d8b2a851
Isabela Abadia <beloz.abadia@gmail.com>
2024-12-18 22:10:56 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,23 @@
 import database from "infra/database.js";
+const databaseName = process.env.POSTGRES_DB;
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "olá" });
+  const updatedAt = new Date().toISOString();
+  const version = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query({
+    text: "SELECT COUNT (*)::int FROM pg_stat_activity WHERE datname= $1 AND state = 'active';",
+    values: [databaseName],
+  });
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count,
+      },
+    },
+  });
 }
 export default status;
