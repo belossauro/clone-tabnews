3c615ef00615d10a47e702385b3bddb3a68da041
Isabela Abadia <beloz.abadia@gmail.com>
2025-04-27 00:03:02 +0000

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,16 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  const client = await getNewClient();
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -11,14 +21,10 @@ async function query(queryObject) {
       process.env.NODE_ENV !== "development" && process.env.NODE_ENV !== "test",
   });
   await client.connect();
-  try {
-    const result = await client.query(queryObject);
-    return result;
-  } finally {
-    await client.end();
-  }
+  return client;
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
